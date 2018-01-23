c10066ebbfaba39e0c6c22fc77ae4ae4da57ed45
roman aznabaev <aznabaevroman@mail.ru>
2018-01-24 00:36:56 +0300

    added Abstract Factory

---
+++ AbstractFactory/AbstractFactorySol/AbstractFactoryApp/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryApp
+{
+    class Client
+    {
+        AbstractProductA apa;
+        AbstractProductB apb;
+
+        public Client(AbstractFactory af)
+        {
+            apa = af.CreateProductA();
+            apb = af.CreateProductB();
+        }
+
+        public void Run()
+        {
+            apb.Interact(apa);
+        }
+    }
+}
---
+++ AbstractFactory/AbstractFactorySol/AbstractFactoryApp/ProductB2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryApp
+{
+    class ProductB2 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA apa)
+        {
+            Console.WriteLine(this + " interacts with " + apa);
+        }
+    }
+}
