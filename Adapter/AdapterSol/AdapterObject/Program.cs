6035536194a24c7572118b900ed8300cc3592827
roman aznabaev <aznabaevroman@mail.ru>
2018-01-23 17:17:44 +0300

    Added pattern Adapter

---
+++ Adapter/AdapterSol/AdapterObject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target target = new Adapter();
+            target.Request();
+            Console.ReadLine();
+        }
+    }
+}
