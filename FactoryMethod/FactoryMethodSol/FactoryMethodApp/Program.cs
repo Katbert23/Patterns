43f21d310b97007df4eaf581277a50ae24588252
roman aznabaev <aznabaevroman@mail.ru>
2018-01-24 01:02:45 +0300

    added FactoryMethod

---
+++ FactoryMethod/FactoryMethodSol/FactoryMethodApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator creator = null;
+            Product product = null;
+
+            creator = new ConcreteCreator();
+            product = creator.FactoryMethod();
+
+            creator.AnOperation();
+
+            Console.ReadKey();
+        }
+    }
+}
