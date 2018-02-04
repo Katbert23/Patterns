9b5d95995ee8d498b38fc4a8e66a377a6e06e47e
roman aznabaev <aznabaevroman@mail.ru>
2018-02-04 21:27:43 +0300

    First commit Prototype

---
+++ Prototype/PrototypeSol/PrototypeApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prototype prototype = null;
+            Prototype clone = null;
+
+            prototype = new ConcretePrototype1(1);
+            clone = prototype.Clone();
+            Console.WriteLine(clone.id);
+
+            Console.WriteLine(new string('*',25));
+
+            prototype = new ConcretePrototype2(2);
+            clone = prototype.Clone();
+            Console.WriteLine(clone.id);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prototype/PrototypeSol/PrototypeApp/Prototype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeApp
+{
+    abstract class Prototype
+    {
+        public int id { get; private set; }
+
+        public Prototype(int id)
+        {
+            this.id = id;
+        }
+
+        public abstract Prototype Clone();
+    }
+}
