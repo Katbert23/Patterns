b27b485ea0edebd3e059ae71d2c7707a687f0fea
roman aznabaev <aznabaevroman@mail.ru>
2018-02-04 21:12:15 +0300

    First commit Singleton

---
+++ Singleton/SingletonSol/SingletonApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance1 = Singleton.Instance();
+            Singleton instance2 = Singleton.Instance();
+
+            Console.WriteLine(ReferenceEquals(instance1,instance2));
+
+            instance1.SingletonOperation();
+            string singletonData = instance1.GetSingletonData();
+            Console.WriteLine(singletonData);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/SingletonSol/SingletonApp/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonApp
+{
+    class Singleton
+    {
+        static Singleton uniqueInstance;
+        string singletonData = string.Empty;
+
+        protected Singleton()
+        { }
+
+        public static Singleton Instance()
+        {
+            if (uniqueInstance == null)
+                uniqueInstance = new Singleton();
+            return uniqueInstance;
+        }
+
+        public void SingletonOperation()
+        {
+            singletonData = "SingletonData";
+        }
+
+        public string GetSingletonData()
+        {
+            return singletonData;
+        }
+    }
+}
